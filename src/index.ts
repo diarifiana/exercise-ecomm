303141ed4b3db640b9e3dac03d4c5c3254746345
diarifiana <diarifiana@gmail.com>
2025-04-10 14:18:36 +0700

    feat: add register (#1)
    * setup db connection
    
    * feat: add register

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { PORT } from "./config/env";
 import { errorMiddleware } from "./middlewares/error.middleware";
 import sampleRouter from "./routes/sample.router";
+import authRouter from "./routes/auth.router";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/samples", sampleRouter);
+app.use("/auth", authRouter);
 
 app.use(errorMiddleware);
 
